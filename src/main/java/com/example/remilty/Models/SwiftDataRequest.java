ef082d52adb1f716053070636f1bfade844901e2
WoyTuloO <wojciech.blicharz192@gmail.com>
2025-05-03 13:28:52 +0200

    added isHeadquarter property to request body

--- src/main/java/com/example/remilty/Models/SwiftDataRequest.java
+++ src/main/java/com/example/remilty/Models/SwiftDataRequest.java
@@ -1,5 +1,6 @@
 package com.example.remilty.Models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,4 +20,7 @@ public class SwiftDataRequest {
     private String countryISO2;
     @NotBlank
     private String countryName;
+    @JsonProperty("isHeadquarter")
+    @NotBlank
+    private boolean isHeadquarter;
 }
