78b2d7b6576280cbb27863bd3fb35832e0ad6ffa
WoyTuloO <wojciech.blicharz192@gmail.com>
2025-05-03 13:28:36 +0200

    updated data loader to use resource class

--- src/main/java/com/example/remilty/DataLoader/SwiftDataLoader.java
+++ src/main/java/com/example/remilty/DataLoader/SwiftDataLoader.java
@@ -5,11 +5,10 @@ import com.example.remilty.Repositories.SwiftRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,35 +24,46 @@ public class SwiftDataLoader implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) {
-        File file = new File("src/main/resources/db.tsv");
-        List<SwiftData> swiftData = loadDbData(file);
-        if (!swiftData.isEmpty())
-            swiftRepository.saveAll(swiftData);
+    public void run(String... args) throws IOException {
+        try(
+            InputStream inputStream = new ClassPathResource("db.tsv").getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        ){
+            List<SwiftData> swiftData = readBufferedReader(reader);
+            if (!swiftData.isEmpty())
+                swiftRepository.saveAll(swiftData);
+        }
     }
 
-    public List<SwiftData> loadDbData(File file) {
-        List<SwiftData> swiftDataList = new ArrayList<>();
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line = br.readLine();
-            while ((line = br.readLine()) != null) {
-                String[] dataSplit = line.split("\t");
 
-                SwiftData swiftData = new SwiftData(
-                        dataSplit[0].trim().toUpperCase(),
-                        dataSplit[1],
-                        dataSplit[3],
-                        dataSplit[4],
-                        dataSplit[6].trim().toUpperCase()
-                );
-
-                if(!swiftRepository.existsBySwiftCode(swiftData.getSwiftCode()))
-                    swiftDataList.add(swiftData);
-
-            }
-        } catch (Exception e) {
+    public List<SwiftData> loadDbData(File file){
+        try (
+            FileInputStream fis = new FileInputStream(file);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+        ) {
+            return readBufferedReader(br);
+        } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public List<SwiftData> readBufferedReader(BufferedReader br) throws IOException {
+        List<SwiftData> swiftDataList = new ArrayList<>();
+        String line = br.readLine();
+        while ((line = br.readLine()) != null) {
+            String[] dataSplit = line.split("\t");
+
+            SwiftData swiftData = new SwiftData(
+                    dataSplit[0].trim().toUpperCase(),
+                    dataSplit[1],
+                    dataSplit[3],
+                    dataSplit[4],
+                    dataSplit[6].trim().toUpperCase()
+            );
+
+            if (!swiftRepository.existsBySwiftCode(swiftData.getSwiftCode()))
+                swiftDataList.add(swiftData);
+
         }
 
         return swiftDataList;
