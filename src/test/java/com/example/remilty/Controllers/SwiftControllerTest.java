09ded5d381150c5252d488073c08524c238b2e21
WoyTuloO <wojciech.blicharz192@gmail.com>
2025-05-04 14:46:34 +0200

    added controller unit tests

--- src/test/java/com/example/remilty/Controllers/SwiftControllerTest.java
+++ src/test/java/com/example/remilty/Controllers/SwiftControllerTest.java
@@ -1,146 +1,232 @@
 package com.example.remilty.Controllers;
 
-import com.example.remilty.Models.SwiftData;
+import com.example.remilty.DTOs.SwiftCountryDataDTO;
+import com.example.remilty.DTOs.SwiftDataDTO;
 import com.example.remilty.Models.SwiftDataRequest;
-import com.example.remilty.Repositories.SwiftRepository;
+import com.example.remilty.Services.SwiftService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.hamcrest.Matchers.startsWith;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
+@WebMvcTest(SwiftController.class)
 public class SwiftControllerTest {
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private ObjectMapper objectMapper;
 
-    @BeforeAll
-    static void setUp() {
-        randomSwiftCode = generateRandomSwiftCode();
-    }
+    @MockBean
+    private SwiftService swiftService;
 
-    @Autowired
-    private SwiftRepository swiftRepository;
-
-    @BeforeEach
-    void initData() {
-        swiftRepository.deleteAll();
-
-        swiftRepository.saveAll(List.of(
-                new SwiftData("PL", "KOPWPLP1XXX", "KOMPANIA PIWOWARSKA S.A.", "UL. SZWAJCARSKA  POZNAN, WIELKOPOLSKIE, 61-285", "POLAND"),
-                new SwiftData("PL", "KOPWPLP1KPW", "KOMPANIA PIWOWARSKA S.A.", "  POZNAN, WIELKOPOLSKIE", "POLAND"),
-                new SwiftData("LV", "AIZKLV22XXX", "ABLV BANK, AS IN LIQUIDATION", "MIHAILA TALA STREET 1  RIGA, RIGA, LV-1045", "LATVIA"),
-                new SwiftData("LV", "BLPBLV21XXX", "AS BALTIJAS PRIVATBANKA", "SIA MONO KATLAKALNA ST. 1 RIGA, RIGA, LV-1017", "LATVIA")
-        ));
-    }
-
-    static String randomSwiftCode;
     @Test
-    void shouldReturnSwiftDataBySwiftCode() throws Exception {
-        String swiftCode2 = "KOPWPLP1XXX";
-        mockMvc.perform(get("/v1/swift-codes/{swiftCode}", swiftCode2))
+    void getExistingSwiftCode() throws Exception {
+        SwiftDataDTO dto = SwiftDataDTO.builder()
+                .swiftCode("ABCDEF12XXX")
+                .bankName("Test Bank")
+                .address("Test Address")
+                .countryISO2("PL")
+                .countryName("POLAND")
+                .isHeadquarter(true)
+                .branches(List.of())
+                .build();
+
+        Mockito.when(swiftService.getSwiftDataBySwiftCode("ABCDEF12XXX"))
+                .thenReturn(Optional.of(dto));
+
+        mockMvc.perform(get("/v1/swift-codes/{swiftCode}", "ABCDEF12XXX"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.swiftCode").value(swiftCode2))
-                .andExpect(jsonPath("$.branches[0].swiftCode").value("KOPWPLP1KPW"));
+                .andExpect(jsonPath("$.swiftCode").value("ABCDEF12XXX"))
+                .andExpect(jsonPath("$.isHeadquarter").value(true));
+
+        verify(swiftService).getSwiftDataBySwiftCode("ABCDEF12XXX");
 
     }
 
     @Test
-    void shouldNotReturnSwiftDataByInvalidSwiftCode() throws Exception {
-        String swiftCode = "ABCDUS33";
+    void getInvalidSwiftCode() throws Exception {
+        String invalidSwiftCode = "INVALI";
+        when(swiftService.getSwiftDataBySwiftCode(invalidSwiftCode)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/v1/swift-codes/{swiftCode}", invalidSwiftCode))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("SWIFT code must be exactly 11 characters"));
+
+    }
+
+    @Test
+    void getExistingCountry() throws Exception {
+        SwiftDataDTO code1 = SwiftDataDTO.builder().swiftCode("ABCUS33XXX").build();
+
+        SwiftCountryDataDTO countryDataDTO = SwiftCountryDataDTO.builder()
+                .countryISO2("US")
+                .countryName("UNITED STATES")
+                .swiftCodes(List.of(code1))
+                .build();
+
+        when(swiftService.getSwiftDataByIso2Code("US")).thenReturn(Optional.of(countryDataDTO));
+
+        mockMvc.perform(get("/v1/swift-codes/country/US"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.countryISO2").value("US"))
+                .andExpect(jsonPath("$.swiftCodes[0].swiftCode").value("ABCUS33XXX"));
+
+        verify(swiftService).getSwiftDataByIso2Code("US");
+    }
+
+
+    @Test
+    void getNonExistingSwiftCode() throws Exception {
+        String swiftCode = "WRONGSWIFTT";
+        when(swiftService.getSwiftDataBySwiftCode(swiftCode)).thenReturn(Optional.empty());
+
         mockMvc.perform(get("/v1/swift-codes/{swiftCode}", swiftCode))
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message").value("No data found for the provided SWIFT code"));
+
+        verify(swiftService).getSwiftDataBySwiftCode(swiftCode);
     }
 
     @Test
-    void shouldReturnSwiftDataByIso2Code() throws Exception {
-        String invalidIso2Code = "LV";
-        mockMvc.perform(get("/v1/swift-codes/country/{iso2Code}", invalidIso2Code))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.countryISO2").value("LV"))
-                .andExpect(jsonPath("$.countryName").value("LATVIA"))
-                .andExpect(jsonPath("$.swiftCodes[0].swiftCode").value("AIZKLV22XXX"))
-                .andExpect(jsonPath("$.swiftCodes[0].address").value("MIHAILA TALA STREET 1  RIGA, RIGA, LV-1045"))
-                .andExpect(jsonPath("$.swiftCodes[0].bankName").value("ABLV BANK, AS IN LIQUIDATION"))
-                .andExpect(jsonPath("$.swiftCodes[0].countryISO2").value("LV"))
-                .andExpect(jsonPath("$.swiftCodes[0].isHeadquarter").value(true))
-                .andExpect(jsonPath("$.swiftCodes[1].swiftCode").value("BLPBLV21XXX"))
-                .andExpect(jsonPath("$.swiftCodes[1].address").value("SIA MONO KATLAKALNA ST. 1 RIGA, RIGA, LV-1017"))
-                .andExpect(jsonPath("$.swiftCodes[1].bankName").value("AS BALTIJAS PRIVATBANKA"))
-                .andExpect(jsonPath("$.swiftCodes[1].countryISO2").value("LV"))
-                .andExpect(jsonPath("$.swiftCodes[1].isHeadquarter").value(true));
+    void getNonExistingCountry() throws Exception {
+        String iso2Code = "WRONGISO2";
+        when(swiftService.getSwiftDataByIso2Code(iso2Code)).thenReturn(Optional.empty());
 
-    }
-
-    @Test
-    void shouldNotReturnSwiftDataByInvalidIso2Code() throws Exception {
-        String iso2Code = "US";
         mockMvc.perform(get("/v1/swift-codes/country/{iso2Code}", iso2Code))
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message").value("No data found for the provided ISO2 code"));
+
+        verify(swiftService).getSwiftDataByIso2Code(iso2Code);
     }
 
     @Test
-    void shouldAddSwiftData_and_shouldReturnConflictWhenDuplicatingData() throws Exception {
-        SwiftDataRequest request = new SwiftDataRequest(
-                randomSwiftCode, "US", "United States", "123 Test St, Test City, Test State, 12345", "ABCDUS33AAA", false);
-        mockMvc.perform(post("/v1/swift-codes")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.message", startsWith("Data saved successfully with ID: ")));
+    void postValidSwiftData() throws Exception {
+        SwiftDataRequest req = new SwiftDataRequest(
+                "ABCDEF12XXX",
+                "Test Bank",
+                "Test Address",
+                "PL",
+                "POLAND",
+                true
+        );
+
+        when(swiftService.addSwiftData(any(SwiftDataRequest.class))).thenReturn(1L);
 
         mockMvc.perform(post("/v1/swift-codes")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
+                        .content(new ObjectMapper().writeValueAsString(req)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message").value(org.hamcrest.Matchers.containsString("Data saved successfully")));
+
+        verify(swiftService).addSwiftData(any(SwiftDataRequest.class));
+
+    }
+
+    @Test
+    void postDuplicatedSwiftData() throws Exception {
+        SwiftDataRequest req = new SwiftDataRequest(
+                "ABCDEF12XXX",
+                "Test Bank",
+                "Test Address",
+                "PL",
+                "POLAND",
+                true
+        );
+
+        when(swiftService.addSwiftData(any(SwiftDataRequest.class))).thenThrow(new DataIntegrityViolationException("SWIFT code already exists"));
+
+        mockMvc.perform(post("/v1/swift-codes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(req)))
                 .andExpect(status().isConflict())
                 .andExpect(jsonPath("$.message").value("SWIFT code already exists"));
+
     }
 
     @Test
-    void shouldReturnNotFoundWhenDeletingNonExistentSwiftData() throws Exception {
-        String swiftCode = "NONEXISTENTCODE";
-        mockMvc.perform(delete("/v1/swift-codes/{swiftCode}", swiftCode))
-                .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.message").value("SWIFT code not found"));
+    void postInvalidSwiftData() throws Exception {
+        SwiftDataRequest req = new SwiftDataRequest(
+                "ABC2XXX",
+                "Test Bank",
+                "Test Address",
+                "PL",
+                "POLAND",
+                true
+        );
+
+
+        mockMvc.perform(post("/v1/swift-codes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(req)))
+                .andExpect(status().isBadRequest());
+
+    }
+
+
+    @Test
+    void postSwiftDataWithUnexpectedError() throws Exception {
+        SwiftDataRequest req = new SwiftDataRequest(
+                "ABCDEF12XXX",
+                "Test Bank",
+                "Test Address",
+                "PL",
+                "POLAND",
+                true
+        );
+
+        when(swiftService.addSwiftData(any(SwiftDataRequest.class))).thenThrow(new RuntimeException("Unexpected error occurred"));
+
+        mockMvc.perform(post("/v1/swift-codes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(req)))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value("Unexpected error occurred"));
+
     }
 
     @Test
-    void shouldDeleteSwiftData() throws Exception {
-        String swiftCode = "KOPWPLP1XXX";
-        mockMvc.perform(delete("/v1/swift-codes/{swiftCode}", swiftCode))
+    void deleteExistingSwiftCode() throws Exception {
+        doNothing().when(swiftService).deleteSwiftData(anyString());
+        mockMvc.perform(delete("/v1/swift-codes/ABCDXXX"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.message").value("SWIFT code deleted successfully"));
+
+        verify(swiftService).deleteSwiftData("ABCDXXX");
     }
 
+    @Test
+    void deleteNonExistingSwiftCode() throws Exception {
+        doThrow(new EntityNotFoundException("SWIFT code not found")).when(swiftService).deleteSwiftData(anyString());
 
-    private static String generateRandomSwiftCode() {
-        StringBuilder swiftCode = new StringBuilder();
-        for (int i = 0; i < 8; i++) {
-            int randomChar = (int) (Math.random() * 26);
-            char c = (char) ('A' + randomChar);
-            swiftCode.append(c);
-        }
-        return swiftCode.toString();
+        mockMvc.perform(delete("/v1/swift-codes/WRONGCODE"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("SWIFT code not found"));
+
+        verify(swiftService).deleteSwiftData("WRONGCODE");
     }
 
+    @Test
+    void deleteSwiftCodeWithUnexpectedError() throws Exception {
+        doThrow(new RuntimeException("Unexpected error")).when(swiftService).deleteSwiftData(anyString());
+
+        mockMvc.perform(delete("/v1/swift-codes/ABCDXXX"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.message").value("Unexpected error occurred"));
+
+        verify(swiftService).deleteSwiftData("ABCDXXX");
+    }
 
 }
